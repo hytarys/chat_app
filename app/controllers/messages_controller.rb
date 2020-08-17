7fe3e50bffe29981c174d6f8a72f27e1830f85a8
hytarys <68804163+hytarys@users.noreply.github.com>
2020-08-17 17:45:10 +0900

    チャットルーム削除機能の実装

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -18,6 +18,7 @@ class MessagesController < ApplicationController
     end
   end
 
+
   private
   
   def message_params
--- app/models/room.rb
+++ app/models/room.rb
@@ -1,6 +1,6 @@
 class Room < ApplicationRecord
   has_many :room_users
-  has_many :messages
-  has_many :users, through: :room_users
+  has_many :messages, dependent: :destroy
+  has_many :users, through: :room_users, dependent: :destroy#roomに紐づいた情報も削除される
   validates :name, presence: true
 end
